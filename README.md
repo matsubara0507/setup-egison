a1ecc3621dff78ee09a07c444c478042f31a6fdb
MATSUBARA Nobutada <nobutada0507@gmail.com>
2020-03-12 01:06:29 +0900

    Feat: setup egison

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # setup-egison
-Set up your GitHub Actions workflow with a specific version of Egison
+
+Set up your GitHub Actions workflow with a specific version of [Egison](https://www.egison.org)
---
+++ src/installer.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function findEgisonVersion(baseInstallDir: string, version: string) {
+  if (!baseInstallDir) {
+    throw new Error('baseInstallDir parameter is required');
+  }
+  if (!version) {
+    throw new Error('versionSpec parameter is required');
+  }
+
+  switch (process.platform) {
+    case 'linux':
+      _findEgisonVersionForLinux(baseInstallDir, version);
+      break;
+    default:
+      throw new Error(`Unsupported OS: ${process.platform}`);
+  }
+}
+
+function _findEgisonVersionForLinux(baseInstallDir: string, version: string) {
+  var tmpOutput = baseInstallDir + '/egison.deb';
+  exec.exec(`wget -o ${tmpOutput} https://git.io/egison.x86_64.deb`);
+  exec.exec(`sudo dpkg -i ${tmpOutput}`);
+  io.rmRF(tmpOutput);
+}
