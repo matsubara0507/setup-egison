3b1d96b5f0f3236e539adf53af40ea33d248d0da
MATSUBARA Nobutada <nobutada0507@gmail.com>
2020-03-12 02:03:32 +0900

    Feat: specify version

--- src/installer.ts
+++ src/installer.ts
@@ -2,7 +2,6 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 import * as fs from 'fs';
-import * as path from 'path';
 
 export function findEgisonVersion(baseInstallDir: string, version: string) {
   if (!baseInstallDir) {
@@ -17,7 +16,7 @@ export function findEgisonVersion(baseInstallDir: string, version: string) {
       _findEgisonVersionForLinux(baseInstallDir, version);
       break;
     default:
-      throw new Error(`Unsupported OS: ${process.platform}`);
+      throw new Error(`unsupported OS: ${process.platform}`);
   }
 }
 
@@ -26,7 +25,12 @@ async function _findEgisonVersionForLinux(
   version: string
 ) {
   var tmpOutput = baseInstallDir + '/egison.deb';
-  await exec.exec(`wget -O ${tmpOutput} https://git.io/egison.x86_64.deb`);
-  await exec.exec(`sudo dpkg -i ${tmpOutput}`);
+  var debUrl = `https://raw.githubusercontent.com/egison/egison-package-builder/${version}/packages/egison.x86_64.deb`;
+  await exec.exec(`wget -O ${tmpOutput} ${debUrl}`);
+  if (fs.existsSync(tmpOutput)) {
+    await exec.exec(`sudo dpkg -i ${tmpOutput}`);
+  } else {
+    throw new Error(`cannot download egison ${version}`);
+  }
   await io.rmRF(tmpOutput);
 }
